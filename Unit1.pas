eec066228435c4c44055d86eb59a8ec850ba2476
katy240700 <katy240700@mail.ru>
2021-02-24 09:57:22 +0700

    first commit

---
+++ Unit1.pas
@@ -0,0 +1,72 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls, Buttons;
+
+type
+  TForm1 = class(TForm)
+    lbFun: TLabel;
+    lbSum: TLabel;
+    pnFun: TPanel;
+    pnSum: TPanel;
+    Bevel1: TBevel;
+    imFun: TImage;
+    imSum: TImage;
+    lbArg: TLabel;
+    edArg: TEdit;
+    rgEps: TRadioGroup;
+    Bevel2: TBevel;
+    Bevel3: TBevel;
+    bbtCalc: TBitBtn;
+    lbFunRes: TLabel;
+    lbSumRes: TLabel;
+    lbNum: TLabel;
+    bbtClose: TBitBtn;
+    procedure bbtCalcClick(Sender: TObject);
+
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses Unit2;
+
+{$R *.dfm}
+
+//Лабораторная работа 11
+//Интерфейс приложения "Вычисление ряда"
+//Выполнила Ефимова Екатерина, гр. БИТ18-11
+
+procedure TForm1.bbtCalcClick(Sender: TObject);
+var
+  x,eps,y,s:Real; //аргумент, погрешность, функция, сумма
+  n:Word; //номер последнего слагаемого
+begin
+  try
+    x:=StrToFloat(edArg.Text);
+    case rgEps.ItemIndex of //проверка выбора элемента в группе радиокнопок
+      0:eps:=1e-3;
+      1:eps:=1e-4;
+      2:eps:=1e-5;
+      else raise Exception.Create('Не выбрано значение погрешности!');
+    end;
+    Calc(x,eps,y,s,n);
+    lbFunRes.Caption:='Функция '+FloatToStrF(y,ffFixed,12,5);
+    lbSumRes.Caption:='Сумма ряда '+FloatToStrF(s,ffFixed,12,5);
+    lbNum.Caption:='Номер последнего слагаемого '+IntToStr(n);
+  except
+   on EConvertError do ShowMessage('Аргумент не введен'#13#10+'или введен с ошибкой!');
+   on E:Exception do ShowMessage(E.Message);
+  end;
+end;
+
+end.
